98b4db7c15655a3fde89c934dcf0bac60eaa5728
laurenkemp <lauren@rotive.io>
2024-01-11 14:10:11 -0800

    added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiKey = "b66fb8b194b4actb07446af800f7eb3o";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {

621446110dfb84a0c84046b9a73eef692b0c2999
laurenkemp <lauren@rotive.io>
2024-01-11 13:54:11 -0800

    added Dictionary component and search form

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import logo from "./images/logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} alt="dictionary-icon" className="App-logo" />
-      <footer>
-        This AI project was coded by
-        <a href="https://laurenkemp.dev" target="_blank">
-          {" "}
-          Lauren Kemp
-        </a>
-        , open-sourced on{" "}
-        <a
-          href="https://github.com/laurenkemp/dictionary-project.git"
-          target="_blank"
-        >
-          GitHub
-        </a>
-        , and hosted on
-        <a href="https://www.netlify.com/" target="_blank">
-          {" "}
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header"></header>
+        <img src={logo} alt="dictionary-icon" className="App-logo" />
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This AI project was coded by
+          <a href="https://laurenkemp.dev" target="_blank" rel="noreferrer">
+            {" "}
+            Lauren Kemp
+          </a>
+          , open-sourced on{" "}
+          <a
+            href="https://github.com/laurenkemp/dictionary-project.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          , and hosted on
+          <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">
+            {" "}
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

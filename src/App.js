00dd330e6d8ae7a2d298bd375004d4b79f01af04
laurenkemp <lauren@rotive.io>
2024-01-11 13:36:11 -0800

    added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ function App() {
     <div className="App">
       <header className="App-header"></header>
       <img src={logo} alt="dictionary-icon" className="App-logo" />
+      <footer>
+        This AI project was coded by
+        <a href="https://laurenkemp.dev" target="_blank">
+          {" "}
+          Lauren Kemp
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/laurenkemp/dictionary-project.git"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        , and hosted on
+        <a href="https://www.netlify.com/" target="_blank">
+          {" "}
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }

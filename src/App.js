5281e6d55eb31cf937453af8d01583be566dd830
laurenkemp <lauren@rotive.io>
2024-01-11 12:38:40 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./images/logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={logo} alt="dictionary-icon" className="App-logo" />
+      Hello World
+    </div>
+  );
+}
+
+export default App;

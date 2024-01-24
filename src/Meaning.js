57ef94a6112fc7d8a4b37408408166a00ae9f3df
laurenkemp <lauren@rotive.io>
2024-01-24 10:15:04 -0800

    added Synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -6,8 +7,12 @@ export default function Meaning(props) {
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>
+        <strong>Definition: </strong>
         {props.meaning.definition}
         <br />
+        <Synonyms synonyms={props.meaning.synonyms} />
+        <br />
+        <strong>Example: </strong>
         {props.meaning.example}
       </p>
     </div>
